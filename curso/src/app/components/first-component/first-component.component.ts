82c671ace62aa9bf4d739755a23037b40c65bdf0
Brennux <nascimentobreno730@gmail.com>
2025-01-15 22:52:14 -0300

    Interpolacao de dados

--- curso/src/app/components/first-component/first-component.component.ts
+++ curso/src/app/components/first-component/first-component.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
-  imports: [],
   templateUrl: './first-component.component.html',
   styleUrl: './first-component.component.css'
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+ name: string = 'Breno';
+ age: number = 23;
+ job = "Programador";
+ hobbies = ['Jogar', 'programar', 'ler'];
+ car = {
+  name: 'Fusca',
+  year: 1969,
+ }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
