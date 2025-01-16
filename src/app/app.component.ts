36c5abd5e01328443e1545b301a0b001d496d106
Brennux <nascimentobreno730@gmail.com>
2025-01-16 17:51:21 -0300

    Compartilhamento de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { ParentDataComponent } from "./components/parent-data/parent-data.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FirstComponentComponent],
+  imports: [FirstComponentComponent, ParentDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  username = 'Jhon Doe';
   title = 'curso';
 }
--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-parent-data',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './parent-data.component.css'
 })
 export class ParentDataComponent {
-
+ @Input() name: string = '';
 }
