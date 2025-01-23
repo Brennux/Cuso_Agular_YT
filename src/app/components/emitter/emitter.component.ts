56fd93e47f388879cc81fe8e9fcd2fce4270704b
Brennux <nascimentobreno730@gmail.com>
2025-01-23 13:33:24 -0300

    emitindo eventos

--- src/app/components/emitter/emitter.component.ts
+++ src/app/components/emitter/emitter.component.ts
@@ -4,18 +4,20 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-emitter',
-  imports: [CommonModule],
+  imports: [CommonModule, ChangeNumberComponent],
   templateUrl: './emitter.component.html',
   styleUrl: './emitter.component.css'
 })
 export class EmitterComponent implements OnInit {
 
+  myNumber: number = 0;
+
   constructor() {}
 
   ngOnInit(): void {
   }
-  onCHangeNumber() {
-    console.log("Deu certo");
+  onChangeNumber() {
+    this.myNumber = Math.floor(Math.random() * 10);
   }
 
 }
